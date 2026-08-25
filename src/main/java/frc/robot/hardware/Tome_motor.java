661efacc36eab848ca86868e249df1c1a5f7cda5
tomer <tomer.hershman@gmail.com>
2026-08-25 20:09:08 +0300

    day1

--- src/main/java/frc/robot/hardware/Tome_motor.java
+++ src/main/java/frc/robot/hardware/Tome_motor.java
@@ -1,13 +1,17 @@
 package frc.robot.hardware;
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.ForwardLimitValue;
 import org.littletonrobotics.junction.Logger;
 
 public class Tome_motor {
     private final TalonFX motor;
 
+
+
     public Tome_motor(TalonFX motor) {
         this.motor = new TalonFX(1);
+
     }
     public void move_half()
     {
@@ -20,26 +24,56 @@ public class Tome_motor {
     public void stop(){
         motor.stopMotor();
     }
-    public String path = "users/downloads/";
-    public void getstuff(){
-        StatusSignal acl = motor.getAcceleration();
-        StatusSignal vol = motor.getMotorVoltage();
-        StatusSignal pos= motor.getPosition();
-        StatusSignal cur= motor.getStatorCurrent();
-        Logger.recordOutput(path+"/vol", vol.hasUpdated());
-        Logger.recordOutput();
-    }public StatusSignal getacl(){
+    public String path = "/users/downloads/";
+
+    public StatusSignal<Double> getacl(){
         StatusSignal acl = motor.getAcceleration();
         return(acl);
 
-    }public StatusSignal get_vel(){
+    }public StatusSignal<Double> get_vel(){
         StatusSignal vel = motor.getVelocity();
         return (vel);
 
+    }public StatusSignal<Double> aura() {
+        StatusSignal pos = motor.getPosition();
+        return (pos);
     }
+    public StatusSignal<Double> get_vol() {
+        StatusSignal vol = motor.getMotorVoltage();
+        return (vol);
+
+    }
+    public StatusSignal<Double> get_cur() {
+        StatusSignal cur= motor.getStatorCurrent();
+        return (cur);
+
+    }
+    public void aurasigma() {
+
+
+        Logger.recordOutput(path + "/current", get_cur().getValue());
+        Logger.recordOutput(path + "/current", get_vol().getValue());
+        Logger.recordOutput(path + "/current", get_vel().getValue());
+        Logger.recordOutput(path + "/current", aura().getValue());
+        Logger.recordOutput(path + "/current", getacl().getValue());
 
 
     }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
