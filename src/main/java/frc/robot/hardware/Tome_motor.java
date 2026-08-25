6fc85dcc319cea2467c16d0ee4a48cae378be403
tomer <tomer.hershman@gmail.com>
2026-08-25 17:23:10 +0300

    no_copilot

---
+++ src/main/java/frc/robot/hardware/Tome_motor.java
@@ -0,0 +1,16 @@
+package frc.robot.hardware;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+public class Tome_motor {
+    private final TalonFX motor;
+
+    public Tome_motor(TalonFX motor) {
+        this.motor = new TalonFX(1);
+    }
+
+
+
+
+}
+
+
