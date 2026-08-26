8c50a9b2a911e57255ff425ca259ad81458b128c
chrissy-wake-up <bill.b.s.sk@gmail.com>
2026-08-26 13:40:56 +0300

    task 6

--- src/main/java/frc/TalonFXTraining.java
+++ src/main/java/frc/TalonFXTraining.java
@@ -2,8 +2,10 @@ package frc;
 
 import com.ctre.phoenix6.CANBus;
 import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.SoftwareLimitSwitchConfigs;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.ConnectedMotorValue;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Voltage;
@@ -18,9 +20,26 @@ public class TalonFXTraining {
 	public TalonFXTraining(int deviceId, CANBus canBus) {
 		this.motor = new TalonFX(deviceId, canBus);
         SoftwareLimitSwitchConfigs slsc = new SoftwareLimitSwitchConfigs();
-        slsc.ForwardSoftLimitEnable=true;
+        /*tasks 5 and 7*/
+		slsc.ForwardSoftLimitEnable=true;
         slsc.ForwardSoftLimitThreshold=360*5;
+		slsc.ReverseSoftLimitEnable=true;
+		slsc.ReverseSoftLimitThreshold=360*3;
         motor.getConfigurator().apply(slsc);
+		/*task 9*/
+		CurrentLimitsConfigs clc = new CurrentLimitsConfigs();
+		clc.SupplyCurrentLimitEnable=true;
+		clc.StatorCurrentLimit=40;
+		motor.getConfigurator().apply(clc);
+
+	}
+
+	private boolean isMotorConnected(){
+		return motor.getConnectedMotor().getValue()!= ConnectedMotorValue.Unknown;
+	}
+
+	public void logMotorConnection(){
+		Logger.recordOutput(logPath+"/isMotorConnected",isMotorConnected());
 	}
 
 	private void setPower(double amount) {
