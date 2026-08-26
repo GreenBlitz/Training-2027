a64ee7022f0040781780a7532a3e681c53610e54
chrissy-wake-up <bill.b.s.sk@gmail.com>
2026-08-26 14:18:00 +0300

    tasks 8, 10 and 11

--- src/main/java/frc/TalonFXTraining.java
+++ src/main/java/frc/TalonFXTraining.java
@@ -3,43 +3,51 @@ package frc;
 import com.ctre.phoenix6.CANBus;
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.SoftwareLimitSwitchConfigs;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.ConnectedMotorValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.units.measure.Current;
 import org.littletonrobotics.junction.Logger;
+import com.ctre.phoenix6.signals.InvertedValue;
 
 
 public class TalonFXTraining {
 
 	private final TalonFX motor;
+	private InvertedValue direction;
 
 	public TalonFXTraining(int deviceId, CANBus canBus) {
 		this.motor = new TalonFX(deviceId, canBus);
-        SoftwareLimitSwitchConfigs slsc = new SoftwareLimitSwitchConfigs();
-        /*tasks 5 and 7*/
-		slsc.ForwardSoftLimitEnable=true;
-        slsc.ForwardSoftLimitThreshold=360*5;
-		slsc.ReverseSoftLimitEnable=true;
-		slsc.ReverseSoftLimitThreshold=360*3;
-        motor.getConfigurator().apply(slsc);
-		/*task 9*/
+		direction = InvertedValue.CounterClockwise_Positive;
+
+		SoftwareLimitSwitchConfigs slsc = new SoftwareLimitSwitchConfigs();
+		/* tasks 5 and 7 */
+		slsc.ForwardSoftLimitEnable = true;
+		slsc.ForwardSoftLimitThreshold = 360 * 5;
+		slsc.ReverseSoftLimitEnable = true;
+		slsc.ReverseSoftLimitThreshold = 360 * 3;
+		motor.getConfigurator().apply(slsc);
+		/* task 9 */
 		CurrentLimitsConfigs clc = new CurrentLimitsConfigs();
-		clc.SupplyCurrentLimitEnable=true;
-		clc.StatorCurrentLimit=40;
+		clc.SupplyCurrentLimitEnable = true;
+		clc.StatorCurrentLimit = 40;
 		motor.getConfigurator().apply(clc);
-
+		/* task 8 */
+		MotorOutputConfigs moc = new MotorOutputConfigs().withInverted(direction);
+		motor.getConfigurator().apply(moc);
 	}
 
-	private boolean isMotorConnected(){
-		return motor.getConnectedMotor().getValue()!= ConnectedMotorValue.Unknown;
+	private boolean isMotorConnected() {
+		return motor.getConnectedMotor().getValue() != ConnectedMotorValue.Unknown;
 	}
 
-	public void logMotorConnection(){
-		Logger.recordOutput(logPath+"/isMotorConnected",isMotorConnected());
+	public void logMotorConnection() {
+		Logger.recordOutput(logPath + "/isMotorConnected", isMotorConnected());
 	}
 
 	private void setPower(double amount) {
@@ -74,13 +82,38 @@ public class TalonFXTraining {
 		return motor.getStatorCurrent();
 	}
 
-    private static final String logPath="/home/linus/roboticsLog";
+	public void reverseMotor() {
+		MotorOutputConfigs moc = new MotorOutputConfigs();
+		moc.withInverted(getMotorInvertedDirection());
+		motor.getConfigurator().apply(moc);
+		direction = getMotorInvertedDirection();
+	}
 
-    public void logAll(){
-        Logger.recordOutput(logPath+"/position",getPosition().getValue());
-        Logger.recordOutput(logPath+"/velocity",getVelocity().getValue());
-        Logger.recordOutput(logPath+"/voltage",getVoltage().getValue());
-        Logger.recordOutput(logPath+"/current",getCurrent().getValue());
-    }
+	public void setNeutralMode(NeutralModeValue neutralMode) {
+		MotorOutputConfigs moc = new MotorOutputConfigs();
+		moc.withNeutralMode(neutralMode);
+		motor.getConfigurator().apply(moc);
+	}
+
+	public void setPosition(double angle) {
+		motor.setPosition(angle);
+	}
+
+	public void setPosition(Angle angle) {
+		motor.setPosition(angle);
+	}
+
+	private InvertedValue getMotorInvertedDirection() {
+		return direction == InvertedValue.Clockwise_Positive ? InvertedValue.CounterClockwise_Positive : InvertedValue.Clockwise_Positive;
+	}
+
+	private static final String logPath = "/home/linus/roboticsLog";
+
+	public void logAll() {
+		Logger.recordOutput(logPath + "/position", getPosition().getValue());
+		Logger.recordOutput(logPath + "/velocity", getVelocity().getValue());
+		Logger.recordOutput(logPath + "/voltage", getVoltage().getValue());
+		Logger.recordOutput(logPath + "/current", getCurrent().getValue());
+	}
 
 }
