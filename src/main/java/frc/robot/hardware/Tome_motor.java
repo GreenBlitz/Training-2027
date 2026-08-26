ba1369e5891913397419e6a1d1843c450e266d10
tomersben <tomer.shamiaben@gmail.com>
2026-08-26 17:26:55 +0300

    did_i_cook

--- src/main/java/frc/robot/hardware/Tome_motor.java
+++ src/main/java/frc/robot/hardware/Tome_motor.java
@@ -1,88 +1,123 @@
 package frc.robot.hardware;
-import com.ctre.phoenix6.StatusSignal;
+
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.SoftwareLimitSwitchConfigs;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.ForwardLimitValue;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 import org.littletonrobotics.junction.Logger;
 
+
 public class Tome_motor {
-    private final TalonFX motor;
+
+	private final TalonFX motor;
+	private final SoftwareLimitSwitchConfigs spin_limit = new SoftwareLimitSwitchConfigs();
+	private final CurrentLimitsConfigs current_limit = new CurrentLimitsConfigs();
+	private InvertedValue Clockwise_Positive = InvertedValue.valueOf(1);
+	private InvertedValue Counter_Clockwise_Positive = null;
 
 
-
-    public Tome_motor(TalonFX motor) {
-        this.motor = new TalonFX(1);
-
-    }
-    public void move_half()
-    {
-        motor.set(0.5);
-
-    }
-    public void move_tenth(){
-        motor.set(0.1);
-    }
-    public void stop(){
-        motor.stopMotor();
-    }
-    public String path = "/users/downloads/";
-
-    public StatusSignal<Double> getacl(){
-        StatusSignal acl = motor.getAcceleration();
-        return(acl);
-
-    }public StatusSignal<Double> get_vel(){
-        StatusSignal vel = motor.getVelocity();
-        return (vel);
-
-    }public StatusSignal<Double> aura() {
-        StatusSignal pos = motor.getPosition();
-        return (pos);
-    }
-    public StatusSignal<Double> get_vol() {
-        StatusSignal vol = motor.getMotorVoltage();
-        return (vol);
-
-    }
-    public StatusSignal<Double> get_cur() {
-        StatusSignal cur= motor.getStatorCurrent();
-        return (cur);
-
-    }
-    public void aurasigma() {
+	public Tome_motor(int id) {
+		this.motor = new TalonFX(id);
+		motor_limit();
+		setCurrent_limit();
+	}
 
 
-        Logger.recordOutput(path + "/current", get_cur().getValue());
-        Logger.recordOutput(path + "/current", get_vol().getValue());
-        Logger.recordOutput(path + "/current", get_vel().getValue());
-        Logger.recordOutput(path + "/current", aura().getValue());
-        Logger.recordOutput(path + "/current", getacl().getValue());
+	public void motor_limit() {
+		spin_limit.ForwardSoftLimitEnable = true;
+		spin_limit.ForwardSoftLimitThreshold = 5.0;
+		spin_limit.ReverseSoftLimitEnable = true;
+		spin_limit.ReverseSoftLimitThreshold = -3.0;
+
+		motor.getConfigurator().apply(spin_limit);
+	}
+
+	public void setCurrent_limit() {
+		current_limit.StatorCurrentLimitEnable = true;
+		current_limit.withStatorCurrentLimit(40);
+		motor.getConfigurator().apply(current_limit);
+	}
+
+	public void move_half() {
+		motor.set(0.5);
+	}
+
+	public void move_tenth() {
+		motor.set(-0.1);
+	}
+
+	public void stop() {
+		motor.stopMotor();
+	}
+
+	public String path = "/users/downloads/";
+
+	public double getacl() {
+		return  motor.getAcceleration().getValueAsDouble();
+	}
+
+	public double get_vel() {
+		return  motor.getVelocity().getValueAsDouble();
+
+	}
+
+	public double get_pos() {
+		return  motor.getPosition().getValueAsDouble();
 
 
-    }
+	}
 
-    }
+	public double get_vol() {
+		return  motor.getMotorVoltage().getValueAsDouble();
+
+	}
+
+	public double get_cur() {
+		return  motor.getStatorCurrent().getValueAsDouble();
+	}
+
+	public void  logger() {
+		Logger.recordOutput(path + "/current", get_cur());
+		Logger.recordOutput(path + "/VOL", get_vol());
+		Logger.recordOutput(path + "/VEL", get_vel());
+		Logger.recordOutput(path + "/POSITION", get_pos());
+		Logger.recordOutput(path + "/ACL", getacl());
+		connected();
+	}
+
+	public void name() {
+		if (Clockwise_Positive == null) {
+			Counter_Clockwise_Positive = null;
+			Clockwise_Positive = InvertedValue.Clockwise_Positive;
+			MotorOutputConfigs motorConfigs = new MotorOutputConfigs();
+			motorConfigs.Inverted = InvertedValue.Clockwise_Positive;
+			motor.getConfigurator().apply(motorConfigs);
+		} else if (Counter_Clockwise_Positive == null) {
+			Clockwise_Positive = null;
+			Counter_Clockwise_Positive = InvertedValue.CounterClockwise_Positive;
+			MotorOutputConfigs motorConfigs = new MotorOutputConfigs();
+			motorConfigs.Inverted = InvertedValue.CounterClockwise_Positive;
+			motor.getConfigurator().apply(motorConfigs);
+		}
+	}
+
+	public void connected() {
+		Logger.recordOutput(path + "/current", motor.isConnected());
+	}
 
 
+	public void mode_switcher(NeutralModeValue mode) {
+		MotorOutputConfigs motorConfig = new MotorOutputConfigs();
+		motorConfig.NeutralMode = mode;
+		motor.getConfigurator().apply(motorConfig);
+	}
+
+	public void set_pos(double pos) {
+		motor.setPosition(pos);
+	}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
